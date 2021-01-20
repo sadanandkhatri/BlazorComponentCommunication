52c3ada6c47fea55d3c761413f00176af4d2f3ba
unknown <ramyankalgi98@gmail.com>
2021-01-20 12:39:13 +0530

    First commit

---
+++ BlazorApp2/BlazorApp2/Shared/PaginationDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorApp2.Shared
+{
+   public class PaginationDTO
+    {
+        public int Page { get; set; } = 1;
+        public int QuantityperPage { get; set; } = 10;
+    }
+}
